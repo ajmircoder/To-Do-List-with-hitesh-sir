c79644597b748459f444c2ff24c93bf5779d23af
ajmir khan <ajmirk01@gmail.com>
2023-10-26 12:08:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { TodoProvider } from './contexts'
+import { TodoForm, TodoItem } from './components';
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos((pvr) => [{ id: Date.now(), ...todo }, ...pvr])
+  };
+
+
+  const updateTodo = (id, todo) => {
+    setTodos((pvr) => pvr.map((p) => p.id === id ? todo : p));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((pvr) => pvr.filter(todo => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(pvr => pvr.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  return (
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md 
+                      rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id}
+                className='w-full'
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  )
+}
